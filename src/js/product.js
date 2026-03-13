2d1e009e0ef72081fb73c3b218557facae65eaa7
sebasss18 <sebasbernals11@gmail.com>
2026-03-13 10:05:22 -0700

    team assignment

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,5 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
@@ -8,13 +8,22 @@ function addProductToCart(product) {
   cart.push(product);
   setLocalStorage("so-cart", cart);
 }
-// add to cart button event handler
+
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+async function loadProduct() {
+  const params = new URLSearchParams(window.location.search);
+  const productId = params.get("product");
+  const product = await dataSource.findProductById(productId);
+  
+  document.querySelector("#addToCart").dataset.id = product.Id;
+}
+
+loadProduct();
+
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
